38eee7e6f1dbf5b93cd2eaee289f19f70242aafe
tha-is <thscostav@gmail.com>
2025-08-05 17:55:04 -0300

    changing html

---
+++ index.js
@@ -0,0 +1,28 @@
+const repositories = document.getElementById("projects-content");
+
+function getAPIGitHubProjects() {
+    fetch('https://api.github.com/users/tha-is/starred')
+    .then(async res => {
+        if( !res.ok) {
+            throw new Error(res.status);
+        }
+
+        let data = await res.json();
+        data.map( item => {
+            let project = document.createElement('div');
+            project.classList.add('m-2');
+            project.innerHTML = `
+    <div class="title"><a href="${item.git_url}" target="_blank">${item.name}</a></div>
+            <div class="description border p-2">${item.description}
+            <span class="badge text-bg-light">${ item.language}</span>
+            </div>
+    
+      
+            `
+
+            repositories.appendChild(project);
+        })
+    })
+}
+
+getAPIGitHubProjects()
